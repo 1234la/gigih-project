49d567c3e446846c5b21a1d68048e910c691efbf
Lathifa Itqon <lathifaitqon@gmail.com>
2022-03-23 14:49:12 +0700

    homework 4: add layout and display data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import data from './Data';
+import './Spotify.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="title">
+        <h1>
+          Spotify Playlist
+        </h1>
       </header>
+      <div className="container">
+        <img
+          src={data.album.images[0].url}
+          alt="image track"
+        />
+        <div className="track-details">
+          <h3 id="title">{data.name}</h3>
+          <p id="artist"><b>Artist : </b>{data.artists[0].name}</p>
+          <p id="album"><b>Album : </b>{data.album.name}</p>
+          <button><a href={data.album.artists[0].external_urls.spotify}>Select</a></button>
+        </div>
+      </div>
     </div>
   );
 }
