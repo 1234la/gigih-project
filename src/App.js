639eb4d1476c155cfadd5989381f9c7f8cb3cd64
Lathifa Itqon <lathifaitqon@gmail.com>
2022-03-28 16:02:52 +0700

    homework 6 : update style card spotify

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import data from './Data';
 import Spotify from './components/Spotify/Spotify';
+import './components/Spotify/Spotify.css';
 
 function App() {
   return (
     <div className="App">
       <header className="title">
-        <h1 style={{textAlign: "center"}}>
+        <h1 style={{textAlign: "center", margin:"30px"}}>
           Spotify Playlist
         </h1>
       </header>
-      {data.map((d)=>(
-        <Spotify name={d.name} 
-        image={d.album.images[0].url} 
-        album={d.album.name} 
-        artist={d.artists[0].name}
-        url={d.album.artists[0].external_urls.spotify}
-      />
-      ))}
+      <div className="wrapper">
+      <div className="cards_wrap">
+        {data.map((d)=>(
+            <Spotify name={d.name} 
+            image={d.album.images[0].url} 
+            album={d.album.name} 
+            artist={d.artists[0].name}
+            url={d.album.artists[0].external_urls.spotify}
+          />
+          ))}
+      </div>
+      </div>
     </div>
   );
 }
--- src/components/Spotify/Spotify.js
+++ src/components/Spotify/Spotify.js
@@ -1,31 +1,28 @@
-import './Spotify.css';
 
 export default function Spotify(props){
     return(
-        <div className="wrapper">
-            <div className="cards_wrap">
-                <div className="card_item">
-                    <div className="card_inner">
-                        <div className="card_top">
-                        <img src={props.image} alt="car" />
+        // <div className="cards_wrap">
+            <div className="card_item">
+                <div className="card_inner">
+                    <div className="card_top">
+                    <img src={props.image} alt="car" />
+                    </div>
+                    <div className="card_bottom">
+                        <div className="card_title">
+                            <b>{props.name}</b>
                         </div>
-                        <div className="card_bottom">
-                            <div className="card_title">
-                                <b>{props.name}</b>
-                            </div>
-                            <div className="card_info">
-                                <p className="artist">{props.artist}</p>
-                                <p className="album">
-                                {props.album}
-                                </p>
-                            </div>
-                            <div className="card_creator">
-                                <button type="button"><a href={props.url}>Select</a></button>
-                            </div>
+                        <div className="card_info">
+                            <p className="artist">{props.artist}</p>
+                            <p className="album">
+                            {props.album}
+                            </p>
+                        </div>
+                        <div className="card_button">
+                            <button type="button"><a href={props.url}>Select</a></button>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        // </div> 
     );
 };
