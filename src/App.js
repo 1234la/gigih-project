57182f6bd7c399e6fec2ceb78f63d518fc9416e3
Lathifa Itqon <lathifaitqon@gmail.com>
2022-04-01 12:13:47 +0700

    homework 8: using useState for select-deselect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Spotify from './components/Spotify/Spotify';
 import { useEffect, useState } from "react";
+import swal from 'sweetalert';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import {InputGroup, FormControl } from 'react-bootstrap/';
 
@@ -23,6 +24,11 @@ const getReturnedParamsFromSpotifyAuth = (hash) => {
 }
 
 function App() {
+  const [token, setToken] = useState("");
+  const [inputVal, setInputVal] = useState("");
+  const [spotifyData, setSpotifyData] = useState([]);
+  const [select, setSelect] = useState([]);
+
   useEffect(() => {
     if(window.location.hash){
       const {access_token, expires_in, token_type} = getReturnedParamsFromSpotifyAuth(window.location.hash);
@@ -34,20 +40,25 @@ function App() {
       localStorage.setItem("tokenType", token_type);
       localStorage.setItem("expiresIn", expires_in);
     }
+
+  //set token
+    setToken(localStorage.getItem("accessToken"));
   },[]);
 
   const handleLogin = () => {
     window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
   };
-  
-  const [inputVal, setInputVal] = useState("");
-  const [spotifyData, setSpotifyData] = useState([]);
 
+  const handleLogout = () => {
+      setToken("");
+      window.localStorage.removeItem("accessToken");
+  };
+  
   //fetch API data
   const getData = async () => {
     console.log("cek input:" + inputVal);
     await fetch(
-      `https://api.spotify.com/v1/search?type=album&include_external=audio&q=${inputVal}`, { 
+      `https://api.spotify.com/v1/search?type=track&include_external=audio&q=${inputVal}`, { 
         method: 'get', 
         headers:{
           'Accept': "application/json",
@@ -58,38 +69,85 @@ function App() {
     ).then((response) => response.json())
       .then((data) => {
         console.log(data);
-        setSpotifyData(data.albums.items);
+        setSpotifyData(data.tracks.items);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err)
+        if (err.response) {
+            console.log("--------------------------------------------------")
+            console.log(err.response.data);
+            console.log(err.response.status);
+            console.log(err.response.headers);
+        } else if (err.request) {
+            console.log("*************************")
+            console.log(err.request);
+        } else {
+
+            console.log("++++++++++++++++++++++++")
+            console.log('Error', err.message);
+        }
+        console.log(err.config);
+        swal("ERROR", "terdapat kesalahan saat mengambil data!", "error");
+      });
       console.log(localStorage.getItem("tokenType")+ " " +localStorage.getItem("accessToken"));
   };
-
+ 
   return (
     <div className="App">
       <header className="title" style={{textAlign: "center"}}>
         <h1 style={{margin:"30px"}}>
-          Search Album in Spotify
+          Search Songs, Artists, or Album on Spotify
         </h1>
+
+      {/* cek login  */}
+      {!token || !window.localStorage.getItem("tokenType") ? 
         <button onClick = {handleLogin} style={{margin:"10px"}}>Log In</button>
+        :  
+        <button onClick = {handleLogout} style={{margin:"10px"}}>Log Out</button> 
+      }
+
+      {token ?  
         <div className="search">
           <input type="text" onChange={(e) => setInputVal(e.target.value)}/>
           <button type="submit" onClick={getData}>
             Search 
           </button>
-      </div>
+        </div>
+        : <p>You Have to Log In Spotify !!!</p>
+      }
+
       </header>
       <div className="wrapper">
         <div className="cards_wrap">
-        {spotifyData.map((d, id)=>(
-            <Spotify 
-              key = {id}
-              name={d.name} 
-              image={d.images[1].url} 
-              total_track = {d.total_tracks} 
-              artist={d.artists[0].name}
-              url={d.external_urls.spotify}
-            />
-            ))}
+        {spotifyData.map((d, id)=>{
+          // {select.map((s) => {
+          //   console.log("ini s:"+s)
+          //   console.log("ini id:"+d.id)
+          //   if(d.id === s){
+          //       setStatus(true)
+          //   }
+          //   else{
+          //       setStatus(false)
+          //       }
+          //   })
+          //   console.log("app.js map select "+status)
+          // }
+          return(
+              <Spotify 
+                key = {id}
+                name={d.name} 
+                image={d.album.images[1].url} 
+                album_name = {d.album.name}
+                total_track_album = {d.album.total_tracks} 
+                artist={d.artists[0].name}
+                url={d.external_urls.spotify}
+                id_item={d.id}
+                selected={select}
+                updateSelect={setSelect}
+              />
+            )
+          })
+        }
         </div>
       </div>
     </div>
--- src/components/Spotify/Spotify.js
+++ src/components/Spotify/Spotify.js
@@ -1,29 +1,58 @@
 import './Spotify.css';
+import {useCallback} from "react";
 
-export default function Spotify(props){
+const Spotify = (props) => {
+
+    const handleUpdateSelect = useCallback(id => {
+        props.updateSelect(id)
+    }, [props.updateSelect])
+
+    //cek selected
+    console.log(props.selected)
+
+    //masukkin id yang diselect
+    const select = (idSelect) => {
+        //ngambil semua elemen dari selected terus ditambahin dengan id item yg baru diselect
+        console.log("selected id : "+idSelect)
+        handleUpdateSelect([...props.selected, idSelect])
+    }
+
+    //masukkin id yg deselect
+    const deselect = (idDeselect) => {
+        const filterSelected = props.selected.filter((spotify) => {return spotify !== idDeselect} );
+        handleUpdateSelect(filterSelected);
+    };
+
     return(
-        // <div className="cards_wrap">
-            <div className="card_item">
-                <div className="card_inner">
-                    <div className="card_top">
-                    <img src={props.image} alt="car" />
+        <div className="card_item">
+            <div className="card_inner">
+                <div className="card_top">
+                <img src={props.image} alt="car" />
+                </div>
+                <div className="card_bottom">
+                    <div className="card_title">
+                        <b>{props.name}</b>
                     </div>
-                    <div className="card_bottom">
-                        <div className="card_title">
-                            <b>{props.name}</b>
-                        </div>
-                        <div className="card_info">
-                            <p className="artist">Artist Name : {props.artist}</p>
-                            <p className="total-tracks"> Total Track : 
-                             {props.total_track}
-                            </p>
-                        </div>
-                        <div className="card_button">
-                            <button className="button_card" type="button"><a href={props.url}>Select</a></button>
-                        </div>
+                    <div className="card_info">
+                        <p className="artist">Artist Name : {props.artist}</p>
+                        <p className="album-name">Album Name : {props.album_name}</p>
+                        <p className="total-tracks"> Total Track of Album : {props.total_track_album} </p>
+                    </div>
+                    <div className="card_button">
+                        {props.selected.includes(props.id_item) ? 
+                            <button className="button_card" type="button" onClick={() => deselect(props.id_item)}>
+                                <a>Deselect</a>
+                            </button>
+                            :
+                            <button className="button_card" type="button" onClick={() => select(props.id_item)}>
+                                <a href={props.url} target="_blank" rel="noreferrer">Select</a>
+                            </button>
+                        }
                     </div>
                 </div>
             </div>
-        // </div> 
+        </div>
     );
 };
+
+export default Spotify;
