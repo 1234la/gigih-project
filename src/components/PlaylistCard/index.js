12c9e891f57a1797a6d49c042699da3245106482
Lathifa Itqon <lathifaitqon@gmail.com>
2022-04-24 18:20:02 +0700

    update minor playlist card

--- src/components/PlaylistCard/index.js
+++ src/components/PlaylistCard/index.js
@@ -12,7 +12,7 @@ const PlaylistCard = (props) => {
                         {props.desc}
                     </p>
                         <a className="block" href={props.url} target="_blank" rel="noreferrer">
-                            <button type="button" className=" inline-block px-6 py-2.5 bg-aqua-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-aqua-400 hover:shadow-lg focus:bg-aqua-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-aqua-500 active:shadow-lg transition duration-150 ease-in-out"> Play </button>
+                            <button type="button" className="inline-block px-6 py-2.5 bg-aqua-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-aqua-400 hover:shadow-lg hover:text-black focus:bg-aqua-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-aqua-500 active:shadow-lg transition duration-150 ease-in-out"> Play </button>
                         </a>
                     
                 </div>
